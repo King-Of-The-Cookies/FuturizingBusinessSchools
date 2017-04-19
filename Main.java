42e908603afb6aa95ad7bfcf63ee083b6b38c3ce
King-Of-The-Cookies <guy.simons@live.nl>
2017-04-19 11:34:37 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,8 +9,8 @@ public class Main {
 
 	public static void main(String[] args) throws TwitterException, ParseException, IOException, SQLException, ClassNotFoundException {
 		// TODO Auto-generated method stub
-		TwitterSetup twitter = new TwitterSetup("QYpCZW54iTLsciRrA950zhPov", "4FMGwrQHEzWLY7XAmOx9tf7ydVjiq29E4QJJt8SHwb3Uv1AIpc",
-				"3515448134-0NWc5GRqLjv71smt72ux7kh57s09meZarnflFNw", "AdBATS664XDkMmRpaHNmKmDBTh9QMmCb3dT3a5LB10Cyq");
+		TwitterSetup twitter = new TwitterSetup("", "",
+				"", "");
 		
 		DBSetup connect = new DBSetup();
 		connect.connectToDB();
